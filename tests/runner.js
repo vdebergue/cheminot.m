cd04f750e20cdcb94c3231558a829750c3c6db4e
Sebastien Renault <sre@zenexity.com>
2014-03-13 09:24:22 +0100

    Add a new test

--- tests/runner.js
+++ tests/runner.js
@@ -12,7 +12,9 @@ var STOPS = {
   'Epernon': 'StopPoint:OCETrain TER-87394114',
   'Rambouillet': 'StopPoint:OCETrain TER-87393314',
   'Versailles-Chantiers': 'StopPoint:OCETrain TER-87393009',
-  'Paris-Montparnasse 1-2': 'StopPoint:OCETrain TER-87391003'
+  'Paris-Montparnasse 1-2': 'StopPoint:OCETrain TER-87391003',
+  'Laval': 'StopPoint:OCETrain TER-87478404',
+  'Le Mans': 'StopPoint:OCETrain TER-87396002'
 };
 
 function asTimeString(time) {
@@ -32,26 +34,47 @@ $.getJSON('http://localhost:9000/api/db/MjAxNC0wMS0xNF8wMC0xOS0wMA==', function(
 
     require(['utils/tdsp/timeRefinement', 'utils/tdsp/pathSelection'], function(tr, ps) {
 
-      describe('timeRefinement', function() {
+      describe('Time refinement', function() {
 
-        it('should return the correct trip from Chartres to Paris-Montparnasse 1-2 at 07:57', function() {
+        describe('From Chartres to Paris-Montparnasse', function() {
 
-          var ts = moment().hours(7).minutes(57).seconds(0).toDate().getTime();
-          var vsId = STOPS['Chartres'];
-          var veId = STOPS['Paris-Montparnasse 1-2'];
-          var x = tr.timeRefinement(db.tdspGraph, vsId, veId, ts);
+          it('at 07:57', function() {
 
-          expect(asTimeString(x[STOPS['Chartres']].gi.arrivalTime)).to.equal('07:46:00');
-          expect(asTimeString(x[STOPS['Maintenon']].gi.arrivalTime)).to.equal('08:09:00');
-          expect(asTimeString(x[STOPS['Epernon']].gi.arrivalTime)).to.equal('08:16:00');
-          expect(asTimeString(x[STOPS['Rambouillet']].gi.arrivalTime)).to.equal('08:29:00');
-          expect(asTimeString(x[STOPS['Versailles-Chantiers']].gi.arrivalTime)).to.equal('08:51:00');
-          expect(asTimeString(x[STOPS['Paris-Montparnasse 1-2']].gi.arrivalTime)).to.equal('09:05:00');
+            var ts = moment().hours(7).minutes(57).seconds(0).toDate().getTime();
+            var vsId = STOPS['Chartres'];
+            var veId = STOPS['Paris-Montparnasse 1-2'];
+            var x = tr.timeRefinement(db.tdspGraph, vsId, veId, ts);
+
+            expect(asTimeString(x[STOPS['Chartres']].gi.arrivalTime)).to.equal('07:46:00');
+            expect(asTimeString(x[STOPS['Maintenon']].gi.arrivalTime)).to.equal('08:09:00');
+            expect(asTimeString(x[STOPS['Epernon']].gi.arrivalTime)).to.equal('08:16:00');
+            expect(asTimeString(x[STOPS['Rambouillet']].gi.arrivalTime)).to.equal('08:29:00');
+            expect(asTimeString(x[STOPS['Versailles-Chantiers']].gi.arrivalTime)).to.equal('08:51:00');
+            expect(asTimeString(x[STOPS['Paris-Montparnasse 1-2']].gi.arrivalTime)).to.equal('09:05:00');
+          });
+
+        });
+
+        describe('From Laval to Chartres', function() {
+
+          it('at 06:17', function() {
+
+            var ts = moment().hours(6).minutes(17).seconds(0).toDate().getTime();
+            var vsId = STOPS['Laval'];
+            var veId = STOPS['Chartres'];
+            var x = tr.timeRefinement(db.tdspGraph, vsId, veId, ts);
+
+            expect(asTimeString(x[STOPS['Laval']].gi.arrivalTime)).to.equal('06:17:00');
+            expect(asTimeString(x[STOPS['Le Mans']].gi.arrivalTime)).to.equal('07:10:00');
+            expect(asTimeString(x[STOPS['Chartres']].gi.arrivalTime)).to.equal('08:48:00');
+          });
+
         });
 
       });
 
       mocha.run();
+
     });
 
   });
