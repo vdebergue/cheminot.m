aeed6c628c50915fe4643ab1ce263e28a1ba3d2b
Sebastien Renault <sre@zenexity.com>
2014-08-23 15:14:51 +0200

    [Android] Read record from database

--- platforms/android/jni/cheminot/cheminot.cpp
+++ platforms/android/jni/cheminot/cheminot.cpp
@@ -3,29 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
-#include <string>
 
-void toto(const char *dbpath) {
+const char* getVersion(const char *dbpath) {
   int retval;
   sqlite3 *handle;
   sqlite3_stmt *stmt;
-  std::string result = "";
+  const char *result = "";
 
   sqlite3_open_v2(dbpath, &handle, SQLITE_OPEN_READONLY, NULL);
-  sqlite3_prepare_v2(handle, "SELECT * FROM CACHE;",-1, &stmt, 0);
+  sqlite3_prepare_v2(handle, "SELECT value FROM cache WHERE KEY = 'version';",-1, &stmt, 0);
   int cols = sqlite3_column_count(stmt);
-
-  while(1) {
-    retval = sqlite3_step(stmt);
-    if(retval == SQLITE_ROW) {
-       for(int col=0 ; col<cols; col++) {
-         const char *r = (const char*)sqlite3_column_text(stmt, col);
-       }
-    } else {
-       break;
-    }
+  retval = sqlite3_step(stmt);
+  if(retval == SQLITE_ROW) {
+    result = (const char *)sqlite3_column_text(stmt, 0);
   }
   sqlite3_close(handle);
+  return result;
 }
 
 extern "C" {
@@ -33,8 +26,9 @@ extern "C" {
 };
 
 JNIEXPORT jstring JNICALL Java_m_cheminot_plugin_jni_CheminotLib_f(JNIEnv *env, jobject obj, jstring dbpath) {
-  jboolean isCopy;
-  const char *path = env->GetStringUTFChars(dbpath, &isCopy);
-  toto(path);
-  return dbpath;
+  const char *path = env->GetStringUTFChars(dbpath, (jboolean *)0);
+  const char *result = getVersion(path);
+  __android_log_print(ANDROID_LOG_DEBUG, "CheminotLog", "RESULTS %s", result);
+  env->ReleaseStringUTFChars(dbpath, path);
+  return env->NewStringUTF(result);
 }
